21590117554219a1d6bb48559b2b014fdcf466fe
duocaleb <duocaleb@gmail.com>
2025-11-06 16:16:54 -0500

    Added Topping Class

---
+++ Topping.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crk_Topping_Scanner
+{
+    internal class Topping
+    {
+        public string ResonantType;
+        public string ToppingType;
+        public string Stat1;
+        public string Stat2;
+        public string Stat3;
+        public float Stat1Value;
+        public float Stat2Value;
+        public float Stat3Value;
+
+        public Topping(string resonantType,
+                       string toppingType,
+                       string stat1,
+                       string stat2,
+                       string stat3,
+                       float stat1Value,
+                       float stat2Value, 
+                       float stat3Value)
+        {
+            if (resonantType == "Not Resonant")
+            {
+                this.ResonantType = "";
+            }
+            else
+            {
+                this.ResonantType = resonantType;
+            }
+            this.ToppingType = toppingType;
+            this.Stat1 = stat1;
+            this.Stat2 = stat2;
+            this.Stat3 = stat3;
+            this.Stat1Value = stat1Value;
+            this.Stat2Value = stat2Value;
+            this.Stat3Value = stat3Value;
+        }
+
+        public override string ToString()
+        {
+            return (this.ResonantType + this.ToppingType + "\n    " + this.Stat1 + ": " + this.Stat1Value + "\n    " + this.Stat2 + ": " + this.Stat2Value + "\n    " + this.Stat3 + ": " + this.Stat3Value);
+        }
+    }
+}
